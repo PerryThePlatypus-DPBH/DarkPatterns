47c7e3dfffed7417a786539e3df8b8c7a2b5c9c2
Ansh Singhal <singhal.ansh0503@gmail.com>
2024-01-12 01:27:25 +0530

    save pred imgs to desired dir

--- utils/helper_functions.py
+++ utils/helper_functions.py
@@ -1,4 +1,14 @@
 from ultralytics import YOLO
+from PIL import Image
+
+
+def get_pred_img(model: YOLO, img_path, save_dir):
+    results = model(img_path)
+    for result in results:
+        im_array = result.plot()
+        im = Image.fromarray(im_array[..., ::-1])
+        im.show()
+        im.save(f'{save_dir}/results.jpg')
 
 
 def get_text_bboxes(model: YOLO, img_path):
@@ -11,4 +21,3 @@ def get_text_bboxes(model: YOLO, img_path):
                 filtered_boxes.append(box.xyxy)
 
     return filtered_boxes
-
